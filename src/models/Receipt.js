e3ff3c77795818e26678ff57ad72909329902bcd
李杨帆 <2531349593@qq.com>
2016-07-25 20:56:26 +0800

    move buildReceiptText function to Receipt.buildReceiptText method

--- src/models/Receipt.js
+++ src/models/Receipt.js
@@ -18,6 +18,32 @@ class Receipt {
     return new Receipt(receiptItems, total, savedTotal);
   }
 
+
+  static buildReceiptText(receipt) {
+
+    const formatMoney = (money) => {
+      return money.toFixed(2);
+    }
+
+
+    let receiptItemsText = receipt.receiptItems
+      .map(receiptItem => {
+        const cartItem = receiptItem.cartItem;
+        return `名称：${cartItem.item.name}，\
+数量：${cartItem.count}${cartItem.item.unit}，\
+单价：${formatMoney(cartItem.item.price)}(元)，\
+小计：${formatMoney(receiptItem.subtotal)}(元)`;
+      })
+      .join('\n');
+
+    return `***<没钱赚商店>收据***
+${receiptItemsText}
+----------------------
+总计：${formatMoney(receipt.total)}(元)
+节省：${formatMoney(receipt.savedTotal)}(元)
+**********************`;
+  }
+
 }
 
 module.exports = Receipt;
