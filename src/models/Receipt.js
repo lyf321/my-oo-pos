a22e256fd9eae151b7fba7c243d075eabb666d2e
李杨帆 <2531349593@qq.com>
2016-07-25 20:49:49 +0800

    move buildReceipt function to Receipt.buildReceipt method

--- src/models/Receipt.js
+++ src/models/Receipt.js
@@ -1,10 +1,23 @@
-class Receipt{
-  constructor(receiptItems,total,savedTotal){
+class Receipt {
+  constructor(receiptItems, total, savedTotal) {
     this.receiptItems = receiptItems;
     this.total = total;
     this.savedTotal = savedTotal;
   }
-  
+
+  static buildReceipt(receiptItems) {
+
+    let total = 0;
+    let savedTotal = 0;
+
+    for (const receiptItem of receiptItems) {
+      total += receiptItem.subtotal;
+      savedTotal += receiptItem.saved;
+    }
+
+    return new Receipt(receiptItems, total, savedTotal);
+  }
+
 }
 
 module.exports = Receipt;
